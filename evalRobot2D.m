89087dd64c3a13ae674273d5189515d2ecf34922
Jensen Khemchandani <jkhemcha@cs.ualberta.ca>
2023-03-21 19:09:47 -0600

    Implemented newton's method

--- evalRobot2D.m
+++ evalRobot2D.m
@@ -1,11 +1,11 @@
 function [pos, J] = evalRobot2D(l, theta)
     % Initialize outputs
-    pos = []
-    J = []
+    pos = [];
+    J = [];
     
     % Find the position vector pos for the end effector [x;y]
     % x
-    pos = [(l(1) * cos(theta(1))) + (l(2) * cos((theta(1) + theta(2))))]
+    pos = [(l(1) * cos(theta(1))) + (l(2) * cos((theta(1) + theta(2))))];
     % y
     pos = [pos; (l(1) * sin(theta(1))) + (l(2) * sin((theta(1) + theta(2))))]
     
--- invKin2D.m
+++ invKin2D.m
@@ -1,4 +1,16 @@
 function theta = invKin2D(l, theta0, pos, n, mode)
-    % Function coded goes here.
+    theta_k = theta0
+    if mode == 0
+        % Newton's method
+        for i = 1:n
+            [curr_pos, J_k] = evalRobot2D(l, theta_k);
+            f_k = curr_pos - pos
+            s_k = -J_k \ f_k;
+            theta_k = theta_k + s_k
+        end
+        theta = theta_k
+    else
+        % Broydin's method
+    end
 end
 
