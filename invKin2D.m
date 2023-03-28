f1162ca53fcb202a2f3fdbe661be0d2e09d30258
Jensen Khemchandani <jkhemcha@cs.ualberta.ca>
2023-03-28 17:39:43 -0600

    Minor improvements to algorithm and added residual norm threshold to stop algorithms

--- invKin2D.m
+++ invKin2D.m
@@ -1,12 +1,20 @@
 function theta = invKin2D(l, theta0, pos, n, mode)
     theta_k = theta0
+    % Define threshold for the norm where we say we have converged
+    threshold = 0.0000001
     if mode == 0
         % Newton's method
         for i = 1:n
             [curr_pos, J_k] = evalRobot2D(l, theta_k);
             f_k = curr_pos - pos
             s_k = -J_k \ f_k;
-            theta_k = theta_k + s_k
+            % Calculate new angles modulus 2 * pi to stop angles from
+            % becoming unnecessarily large.
+            theta_k = mod((theta_k + s_k), 2 * pi)
+             % Stop at threshold for residual norm
+            if norm(f_k) < threshold
+                break
+            end
         end
         theta = theta_k
     elseif mode == 1
@@ -16,12 +24,18 @@ function theta = invKin2D(l, theta0, pos, n, mode)
         for i = 1:n
             f_k = curr_pos - pos
             s_k = -B_k \ f_k
-            theta_k = theta_k + s_k
+            % Calculate new angles modulus 2 * pi to stop angles from
+            % becoming unnecessarily large.
+            theta_k = mod((theta_k + s_k), 2 * pi)
             % Evaluate the new position with theta k+1
             curr_pos = evalRobot2DNoJacobian(l, theta_k)
             f_k_1 = curr_pos - pos
             y_k = f_k_1 - f_k
             B_k = B_k + (((y_k - (B_k * s_k)) * s_k') / (s_k' * s_k))
+            % Stop at threshold for residual norm
+            if norm(f_k) < threshold
+                break
+            end
         end
         theta = theta_k
     end
